c9d5f87906fcc39b6363c96ba57df9e533190fe3
Madhan kumar <36563503+madhankarky707@users.noreply.github.com>
2018-03-08 15:24:40 +0530

    Create Test.sol

---
+++ Test.sol
@@ -0,0 +1,50 @@
+pragma solidity^0.4.0;
+contract ERC20
+{
+    function transfer(address to,uint256 amount)public;
+    function transferfrom(address from,address to,uint256 amount)public;
+    function mint(uint256 amount)public returns(uint256);
+}
+contract test is ERC20
+{
+    string name;
+    string symbol;
+    uint256 decimal;
+    uint256 totalsupply;
+    address owner;
+    function test()
+    {
+        name="Mahendra Next Wealth";
+        symbol="MNW";
+        decimal=18;
+        totalsupply=1000; 
+        owner=msg.sender;
+    }
+  
+    mapping(address=>uint256)balance;
+    modifier check()
+    {
+        require(owner==msg.sender);
+        _;
+    }
+    function transfer(address to,uint256 amount)check()public
+    {
+        require(amount<=balance[msg.sender]&&balance[msg.sender]!=0);
+     balance[msg.sender]-=amount;
+     balance[to]+=amount;
+    }
+    function transferfrom(address from,address to,uint256 amount)public
+    {
+        require(amount<=balance[msg.sender]&&balance[msg.sender]!=0);
+        balance[from]=amount;
+        balance[msg.sender]-=amount;
+        balance[to]+=amount;
+    }
+    function mint(uint256 amount)check()public returns(uint256)
+    {
+        require(amount<=totalsupply);
+        balance[msg.sender]+=amount;
+        require(balance[msg.sender]<=totalsupply);
+        return balance[msg.sender];
+    }
+}
